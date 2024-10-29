a9bb399f6b67db50dc86e85c7f604b255c438c44
MatheusMoraesMax <maxplaybrhd@gmail.com>
2024-10-28 21:51:10 -0300

    Add files via upload

---
+++ 28 10 web/js/meuscript.js
@@ -0,0 +1,6 @@
+function lIGAR(){
+    document.getElementById ('lamp').src="https://www.w3schools.com/js/pic_bulbon.gif";
+}
+function apagar(){
+    document.getElementById ('lamp').src="https://www.w3schools.com/js/pic_bulboff.gif";
+}
