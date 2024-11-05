b4da3f0ac62011bf3245ed2ac8cbef0830832e44
MatheusMoraesMax <maxplaybrhd@gmail.com>
2024-11-04 21:45:36 -0300

    Add files via upload

---
+++ aula 04 11 2024/java.js
@@ -0,0 +1,17 @@
+function mudaTamanho(){
+    document.getElementById('texto').style.fontSize ='20px'
+}
+function ocultar(){
+document.getElementById('texto').style.display ='none'||'block';  
+    
+}
+function exibir(){
+    document.getElementById('texto').style.display ='block';
+}
+function mensagem(){
+    alert('!Esta é uma mensagem!')
+}
+function Salvar(){
+    var nome=document.getElementById('n').value;
+    alert ("Usuario de Droga= "+nome+"!!!");
+}
